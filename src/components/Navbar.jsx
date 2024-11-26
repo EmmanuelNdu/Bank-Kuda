c49872d2c79103ef4e148d95b085dd30510b13c2
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-26 11:41:29 +0100

    small screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import {React, useState} from 'react'
+import { Menu, X } from 'lucide-react'
 import logo from '../assets/logo.png'
 import { navItems } from '../constants'
 
 const Navbar = () => {
+
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false)
+
+    const toggleNavbar = () => {
+        setMobileDrawerOpen( !mobileDrawerOpen);
+    }
+
   return (
     <nav className='sticky top-0 z-50 py-3 shadow-bar_shadow border-neutral-700/80 '>
         <div className='container px-4 mx-auto relative text-sm '>
@@ -27,7 +35,24 @@ const Navbar = () => {
                         Join Kuda
                     </a>
                 </div>
+                <div className='lg:hidden md:flex flex-col justify-end'>
+                    <button onClick={toggleNavbar}>
+                        {mobileDrawerOpen ? <X/> : <Menu />}
+                    </button>
+                </div>
             </div>
+            {mobileDrawerOpen && (
+                <div className='fixed right-0 z-20 bg-neutral-900 w-full p-12 
+                flex flex-col justify-center items-center lg:hidden'>
+                    <ul>
+                        {navItems.map((item, index) => (
+                            <li key={index}>
+                                <a href={item.href}>{ item.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     </nav>
   )
