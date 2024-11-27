1072e961c8c29a01fe346b56fcd08fca6f2880d6
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-27 13:25:18 +0100

    updated the background

--- src/components/MoneyApp.jsx
+++ src/components/MoneyApp.jsx
@@ -5,10 +5,10 @@ import React from 'react'
 const MoneyApp = () => {
   return (
     <div className=' firstSection flex flex-col mt-64 ml-64 '>
-        <h1 className=' font-black text-5xl text-[#40196d]'>The money app for<br/>
+        <h1 className=' font-extrabold text-5xl text-[#40196d]'>The money app for<br/>
             Africans  <br/>
             </h1>
-        <p className='mt-10 font-semibold'>Make free transfers, enjoy cashless payment <br/>
+        <p className='mt-10 font-semibold text-xl tracking-wide'>Make free transfers, enjoy cashless payment <br/>
             options and earn interest on your savings with <br/>
             Kuda.
             </p>
