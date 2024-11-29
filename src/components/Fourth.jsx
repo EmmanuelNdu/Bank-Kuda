861d49d3b5133591e44a0f63b1aefc1a8de277d9
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-29 22:19:09 +0100

    text

--- src/components/Fourth.jsx
+++ src/components/Fourth.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Fourth = () => {
   return (
-    <div className='flex'>
+    <div className='flex mt-32'>
         <div className='ml-auto mr-64'>
-        <h1 className=' font-bold text-4xl text-[#40196d] mt-64 justify-end'>It’s your money, we just<br/>
+        <h1 className=' font-black text-6xl text-[#40196d] mt-64 justify-end'>It’s your money, we just<br/>
              help you manage it.</h1>
-             <p className='mt-10 font-normal  text-lg tracking-tight'>Save it, spend it, send it. It’s up to you.<br/>
+             <p className='mt-10 font-normal  text-semibold tracking-tight'>Save it, spend it, send it. It’s up to you.<br/>
               Whatever you choose to do with your money,<br/>
                we’ll make sure it’s done better and free of<br/>
                 charge. We take responsibility for that.</p>
