015bfc60a1a60332f20497c3cbd5aa105737d629
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-29 22:06:05 +0100

    chnages

--- src/components/Fourth.jsx
+++ src/components/Fourth.jsx
@@ -6,7 +6,7 @@ const Fourth = () => {
         <div className=''>
         <h1 className=' font-bold text-4xl text-[#40196d] mt-64 justify-end'>It’s your money, we just<br/>
              help you manage it.</h1>
-             <p className='mt-10 font-normal  text-lg tracking-tight justify-items-end'>Save it, spend it, send it. It’s up to you.<br/>
+             <p className='mt-10 font-normal  text-lg tracking-tight'>Save it, spend it, send it. It’s up to you.<br/>
               Whatever you choose to do with your money,<br/>
                we’ll make sure it’s done better and free of<br/>
                 charge. We take responsibility for that.</p>
