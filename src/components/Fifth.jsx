ca3b138dc098bf69768ec89c52ce33b3c6750b19
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-30 20:42:09 +0100

    text

--- src/components/Fifth.jsx
+++ src/components/Fifth.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { ChevronRight } from 'lucide-react'
+import lady from '../assets/lady.png'
 
 const Fifth = () => {
   return (
-    <div>Fifth</div>
+    <div className='flex  flex-wrap mt-10 lg:mt-48 ml-4 '>
+      <div className='ml-60'>
+        <h1 className='font-extrabold text-4xl text-[#40196d] leading-snug'>
+        Save money as you <br />
+        spend it, seriously.
+        </h1>
+        <p className='mt-10 font-normal  text-lg tracking-tight'>
+        We designed a money app for your lifestyle,<br />
+         and you can get a debit card to go with it.<br />
+          That's all you need to make the right money<br />
+           moves.
+        </p>
+        <a className='underline inline-flex text-[#40196d] font-semibold' href='https://account.com'>Open An Account in Minutes<ChevronRight/></a>
+        </div>
+        <img  className='ml-48' src={lady} />
+    </div>
   )
 }
 
