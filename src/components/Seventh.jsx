efdc904bac0662e50bf4a9eed51ca4c0eebc272f
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-30 21:46:19 +0100

    seventh

--- src/components/Seventh.jsx
+++ src/components/Seventh.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { ChevronRight } from 'lucide-react'
-import purple from '../assets/purple.png'
+import lady from '../assets/lady.png'
 
-const Sixth = () => {
+const Seventh = () => {
   return (
     <div className='flex  flex-wrap mt-10 lg:mt-48 ml-4 '>
-         <img  className='ml-48' src={purple} />
       <div className='ml-60'>
         <h1 className='font-extrabold text-4xl text-[#40196d] leading-snug'>
-        Save money as you <br />
-        spend it, seriously.
+        We’re always happy to<br />
+        help you.
         </h1>
         <p className='mt-10 font-normal  text-lg tracking-tight'>
         You can’t avoid spending. That’s how you pay <br />
@@ -20,8 +19,9 @@ const Sixth = () => {
         </p>
         <a className='underline inline-flex text-[#40196d] font-semibold' href='https://account.com'>See all our Savings<ChevronRight/></a>
         </div>
+        <img  className='ml-48' src={lady} />
     </div>
   )
 }
 
-export default Sixth
+export default Seventh
