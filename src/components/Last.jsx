957b28058b6efea33183b477262d0eb4bb3110de
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-02 21:11:59 +0100

    last

--- src/components/Last.jsx
+++ src/components/Last.jsx
@@ -3,7 +3,7 @@ import MyCarousel from './MyCarousel'
 
 const Last = () => {
   return (
-    <div className=' flex justify-center '>
+    <div className=' flex justify-center border-white '>
         <p className='font-black text-4xl text-[#40196d] leading-snug mb-20'>Don’t just take our word for it</p>
         <div className='pb-32'>
         < MyCarousel />
