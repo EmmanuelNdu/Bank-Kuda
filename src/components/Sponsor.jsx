62d16bd0b9f30bd06eae53f2f0404424350ab543
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-04 23:54:53 +0100

    images

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import techcrunch from '../assets/techcrunch.png'
+import eco from '../assets/eco.png'
+import fintech2 from '../assets/fintech2.png'
+import bbc from '../assets/bbc.png'
+import cnbc from '../assets/cnbc.png'
+import euro from '../assets/euro.png'
 
 const Sponsor = () => {
   return (
-    <div>Sponsor</div>
+    <div className='flex'>
+      <img src={techcrunch} />
+      <img src={eco} />
+      <img src={fintech2} />
+      <img src={bbc} />
+      <img src={cnbc} />
+      <img src={euro} />
+
+    </div>
   )
 }
 
