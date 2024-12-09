2aed665ee328cdb4ac6f624f0a3d6400c827a900
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-10 00:17:42 +0100

    lish

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const More = () => {
   return (
     <div>
-        <h1 className='flex font-black text-4xl text-[#40196d] leading-snug mb-20 justify-center mt-40'>More for you</h1>
+        <h1 className='flex font-black text-4xl text-[#40196d] leading-snug mb-20 justify-center mt-12'>More for you</h1>
     </div>
   )
 }
