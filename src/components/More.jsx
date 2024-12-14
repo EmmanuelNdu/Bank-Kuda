883ef3cc1fc356d1d739e6fc7a0522e28862147f
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-14 11:13:44 +0100

    changes

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -6,9 +6,9 @@ const More = () => {
   return (
     <div className=''>
         <h1 className='flex font-black text-4xl text-[#40196d] leading-snug mb-20 justify-center mt-12'>More for you</h1>
-        <div className='flex pl-60 pr-60'>
+        <div className='flex pl-60 pr-60 space-x-8'>
           {kuda_business.map((Option, index) =>(
-            <div key={index} className='w-full' > 
+            <div key={index} className='w-full flex-grow-0' > 
             <div className='p-10 border shadow-bar_shadow rounded-xl' >
               <p className='font-black text-2xl text-[#40196d] pt-6'>
                 {Option.title}
